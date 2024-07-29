f57dbb188e26f563a77e879109d60191be737cf1
Julimartin <martin.julieta@hotmail.com>
2024-07-29 20:44:14 -0300

    Se adiciona la limitación de tipo de caracteres + refactorización de código

--- script.js
+++ script.js
@@ -1,78 +1,89 @@
+// Obtención de elementos del DOM
 const botonEncriptar = document.getElementById("btn_encriptar");
 const botonDesencriptar = document.getElementById("btn_desencriptar");
 const botonCopiar = document.getElementById('btn_copiar');
+const textoInput = document.getElementById("texto");
+const textoResultadoElement = document.getElementById('textoResultado');
+const imgRespuesta = document.getElementById('img-respuesta');
+const instruccion = document.getElementById('instrucción');
+const frase = document.getElementById('frase');
 
 
-function traducirTexto(textoEncriptado, reglas) {
-    for (let item in reglas) {
+const reglasEncriptar = {
+    "e": "enter",
+    "i": "imes",
+    "a": "ai",
+    "o": "ober",
+    "u": "ufat"
+};
+
+const reglasDesencriptar = {
+    "enter": "e",
+    "imes": "i",
+    "ai": "a",
+    "ober": "o",
+    "ufat": "u"
+};
+
+
+function traducirTexto(texto, reglas) {
+    for (const item in reglas) {
         if (reglas.hasOwnProperty(item)) {
-            const traduccion = reglas[item];
-            textoEncriptado = textoEncriptado.split(item).join(traduccion);
+            texto = texto.split(item).join(reglas[item]);
         }
     }
-    return textoEncriptado;
+    return texto;
 }
 
-function encriptar(texto) {
 
-    const reglas = {
-        "e": "enter",
-        "i": "imes",
-        "a": "ai",
-        "o": "ober",
-        "u": "ufat"
-    };
-    return traducirTexto(texto, reglas);
+const encriptar = texto => traducirTexto(texto, reglasEncriptar);
+const desencriptar = texto => traducirTexto(texto, reglasDesencriptar);
+
+
+function validarTexto(texto) {
+    const regex = /^[a-z\s]+$/;
+    return regex.test(texto);
 }
 
-function desencriptar(texto) {
-
-    const reglas = {
-        "enter": "e",
-        "imes": "i",
-        "ai": "a",
-        "ober": "o",
-        "ufat": "u"
-    };
-    return traducirTexto(texto, reglas);
-}
 
 function ocultarElementos() {
-    document.getElementById('img-respuesta').classList.add('oculto');
-    document.getElementById('instrucción').classList.add('oculto');
-    document.getElementById('frase').classList.add('oculto');
+    imgRespuesta.classList.add('oculto');
+    instruccion.classList.add('oculto');
+    frase.classList.add('oculto');
 }
 
+
 function mostrarTexto(funcion) {
-    ocultarElementos();
-    const texto = document.getElementById("texto").value;
-    const textoResultado = funcion(texto);
-    const textoResultadoElement = document.getElementById('textoResultado');
-    
-    
-    // Reiniciar la animación
-    textoResultadoElement.classList.remove('fadeIn');
-    void textoResultadoElement.offsetWidth; // Forzar reflujo
-    textoResultadoElement.classList.add('fadeIn');
+    const texto = textoInput.value;
 
-    textoResultadoElement.textContent = textoResultado;
+    if (!validarTexto(texto)) {
+        alert("Solo letras minúsculas sin acentos ni caracteres especiales.");
+    } else {
+        ocultarElementos();
+        const textoResultado = funcion(texto);
+        
+        // Reiniciar la animación
+        textoResultadoElement.classList.remove('fadeIn');
+        void textoResultadoElement.offsetWidth; // Forzar reflujo
+        textoResultadoElement.classList.add('fadeIn');
 
-    // Mostrar el botón de copiar si hay un resultado
-    if (textoResultado) {
-        botonCopiar.classList.remove('oculto');
+        textoResultadoElement.textContent = textoResultado;
+
+
+        if (textoResultado) {
+            botonCopiar.classList.remove('oculto');
+        }
     }
 }
 
+
 function copiarTexto() {
-    const textoResultado = document.getElementById('textoResultado').textContent;
+    const textoResultado = textoResultadoElement.textContent;
     navigator.clipboard.writeText(textoResultado)
-        .then(() => {
-            alert("Texto copiado al portapapeles");
-        })
-        .catch(err => {
-            console.error('Error al copiar el texto: ', err);
-        });
+        .then(() => alert("Texto copiado al portapapeles"))
+        .catch(err => console.error('Error al copiar el texto: ', err));
 }
+
 
 botonEncriptar.addEventListener('click', () => mostrarTexto(encriptar));
 botonDesencriptar.addEventListener('click', () => mostrarTexto(desencriptar));
